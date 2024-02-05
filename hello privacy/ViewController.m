5fb4e13d3c4affac80b2e85f4ae22598d60efe29
rajat <rajat.temaniya@gmail.com>
2024-02-05 17:35:54 +0530

    Initial Commit

--- hello privacy/ViewController.m
+++ hello privacy/ViewController.m
@@ -9,13 +9,53 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UILabel *instructionLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    //[super viewDidLoad];
     // Do any additional setup after loading the view.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(screenshotTaken)
+                                                     name:UIApplicationUserDidTakeScreenshotNotification
+                                                   object:nil];
+    NSLog(@"Hello, From view did load!");
+    
+    // Add instruction label
+    [self addInstructionLabel];
+   
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    // Manually call screenshotTaken for testing purposes, Uncomment below line for testing
+    //[self screenshotTaken];
+}
+
+
+- (void)addInstructionLabel {
+    self.instructionLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, CGRectGetWidth(self.view.frame), 50)];
+    self.instructionLabel.text = @"Take the screenshot";
+    self.instructionLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.instructionLabel];
+}
+
+
+- (void)screenshotTaken {
+    // Respond to screenshot event here
+    // For example, you can present an alert or disable user interaction
+    NSLog(@"Hello, From Screenshoot deltected");
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Screenshot Detected" message:@"Screenshots are not allowed in this app." preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 
--- hello privacy/main.m
+++ hello privacy/main.m
@@ -13,6 +13,7 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        NSLog(@"Hello, From Main:");
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
